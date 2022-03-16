484b32fb5934121d3497596eef94835910fe0a4a
Kirito1987 <47379527+Kirito1987@users.noreply.github.com>
2022-03-16 01:29:10 -0500

    Add files via upload

---
+++ RPSgame/main.py
@@ -0,0 +1,53 @@
+import random
+
+while True:
+    choices = ["rock", "paper", "scissors"]
+
+    computer = random.choice(choices)
+    player = None
+
+    while player not in choices:
+        player = input("rock, paper, or scissors?: ").lower()
+
+    if player == computer:
+        print("computer: ", computer)
+        print("player: ", player)
+        print("Tie!!")
+    elif player == "rock":
+        if computer == "paper":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("Computer Wins! Try Again")
+        else:
+            if computer == "scissors":
+                print("computer: ", computer)
+                print("player: ", player)
+                print("Player Wins!!")
+    elif player == "paper":
+        if computer == "paper":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("Computer Wins! Try Again")
+        else:
+            if computer == "scissors":
+                print("computer: ", computer)
+                print("player: ", player)
+                print("Player Wins!!")
+    elif player == "scissors":
+        if computer == "rock":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("Computer Wins! Try Again")
+        else:
+            if computer == "scissors":
+                print("computer: ", computer)
+                print("player: ", player)
+                print("Player Wins!!")
+
+    play_again = input("Play again? (yes/no): ").lower()
+
+    if play_again != "yes":
+        break
+print("Thanks for playing!!")
+
+# print(computer)
